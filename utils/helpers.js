c8d42cf86ed0d1bba2de9b8bbc79146e7f8f45bb
Daisy Lien <daisylien1211@gmail.com>
2022-10-17 19:36:50 -0700

    chnge

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,8 @@
 module.exports = {
     get_icon: () => {
-<<<<<<< HEAD
-=======
         const score = Math.max(100);
->>>>>>> d701d328cfe52aeb2c4f5be8e4f96268dc0e34d6
         // TODO HERE!!!!
-        const score = Math.random();
+
         if (score < 40) {
             return '<img src="/public/images/angrydog.jpg">';
         } else if (score > 80) {
@@ -14,8 +11,4 @@ module.exports = {
             return '<img src="/public/images/okdog.jpg">';
         }
     },
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> d701d328cfe52aeb2c4f5be8e4f96268dc0e34d6
