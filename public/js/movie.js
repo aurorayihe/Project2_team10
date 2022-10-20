ba617abd7f323e4a24ab204fdbb61984e79d0339
Daisy Lien <daisylien1211@gmail.com>
2022-10-19 17:05:52 -0700

    fix layout and fix review

--- public/js/movie.js
+++ public/js/movie.js
@@ -1,14 +1,17 @@
 const newReviewHandler = async (event) => {
     event.preventDefault();
     // const movieTitle = document.querySelector('#movie_title').value.trim();
-    const score = document.querySelector('#score').value.trim();
-    const comment = document.querySelector('#comment').value.trim();
-    const movie_id = document.querySelector('#movieID').value;
-    console.log(movie_id);
+    // const score = document.querySelector('#score').value.trim();
+    // const comment = document.querySelector('#comment').value.trim();
+    // const movie_id = document.querySelector('#movieID').value;
+    const movieId = document.querySelector('input[name="movie-id"]').value;
+    const comment = document.querySelector('textarea[name ="comment"]').value;
+    const score = document.querySelector('textarea[name="score"]').value;
+    
     if (score && comment) {
         const newReview = await fetch(`/api/reviews/`, {
             method: 'POST',
-            body: JSON.stringify({score, comment, movie_id}),
+            body: JSON.stringify({score, comment, movieId}),
             headers: {
                 'Content-Type': 'application/json',
             },
